2ce2cecf4ea27180dcdd38329a8bd133f51d6634
Shiviara <113165919+Shiviara@users.noreply.github.com>
2022-10-21 16:26:16 +0200

    nav bar - now in js

---
+++ js.js
@@ -0,0 +1,18 @@
+/**
+ * file: js.js
+ */
+ console.log("Hi from js.js")
+
+ //nav bar
+const NavBar = `
+<div class="sidenav">
+    <a href="index.html">Home</a>
+    <a href = "calendar">Calendar</a>
+    <a href="announcments.html">Announcments</a>
+    <a href="facilities.html">Facilities</a>
+    <a href="ideapost.html">Ideas</a>
+    <a href = "contact.html">Contact</a>
+    </div>
+`
+
+ document.getElementById("nav").innerHTML= NavBar
