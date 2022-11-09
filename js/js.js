b454a64a597476df70ff448223ab48e73c48ddb9
Shiviara <113165919+Shiviara@users.noreply.github.com>
2022-11-09 13:41:42 +0100

    front page for mobile works

--- js/js.js
+++ js/js.js
@@ -6,7 +6,7 @@
  //nav bar
 const NavBar = `
 <div class="sidenav">
-    <a href="../front-page/index.html"><img src="../images/icons/home.png" class="icon"></a>
+    <a href="../front-page/index.html"><img src="../images/icons/home.png" class="icon" id="home"></a>
     <a href ="../calendar/calendar.html"><img src="../images/icons/nav.cal.png" class="icon"></a>
     <a href="../announcements/announcements.html"><img src="../images/icons/nav.an.png" class="icon"></a>
     <a href="../facilities/facilities.html"><img src="../images/icons/nav.map.png" class="icon"></a>
