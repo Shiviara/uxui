2439695fcec3aa8b7b983b7f1b67cc26ff5749bf
varazanina <113165723+varazanina@users.noreply.github.com>
2022-11-18 08:06:09 +0100

    fac

--- facilities/js.js
+++ facilities/js.js
@@ -5,3 +5,27 @@ function on() {
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
+
+  function on1() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off1() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  function on2() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off2() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+  function on3() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off3() {
+    document.getElementById("overlay").style.display = "none";
+  }
