73deeb9ffa66b44ce805a0feff9695f8b6e45695
Shiviara <113165919+Shiviara@users.noreply.github.com>
2022-11-24 00:03:48 +0100

    test

--- js.js
+++ js.js
@@ -6,8 +6,8 @@
  //nav bar
 const NavBar = `
 <div class="sidenav">
-    <a href="../index.html"><img src="../images/icons/home.png" class="icon" id="home" alt="home page"></a>
-    <a href ="../calendar/calendar.html"><img src="../images/icons/nav.cal.png" class="icon" alt="calendar"></a>
+    <a href="index.html"><img src="../images/icons/home.png" class="icon" id="home" alt="home page"></a>
+    <a href ="calendar.html"><img src="../images/icons/nav.cal.png" class="icon" alt="calendar"></a>
     <a href="../announcements/announcements.html"><img src="../images/icons/nav.an.png" class="icon" alt="announcements"></a>
     <a href="../facilities/facilities.html"><img src="../images/icons/nav.map.png" class="icon" alt="facilities"></a>
     <a href="../ideas/ideas.html"><img src="../images/icons/nav.id.png" class="icon" alt="post ideas"></a>
